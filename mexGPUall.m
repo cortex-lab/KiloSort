d1cd8ea3ca5360fa17d46a5baa453d9e263e4b44
nsteinme <nick.steinmetz@gmail.com>
2016-01-26 11:48:13 +0000

    added rezToPhy

--- mexGPUall.m
+++ mexGPUall.m
@@ -4,3 +4,5 @@ mex -largeArrayDims mexWtW.cu
 mex -largeArrayDims mexMPreg.cu
 mex -largeArrayDims mexMPsub.cu
 mex -largeArrayDims mexMPmuFEAT.cu
+mex -largeArrayDims mexMPregMU.cu
+mex -largeArrayDims mexWtW2.cu
