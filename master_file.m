43dd09c51945d91c43b4affd8ddbfe347e32b369
marius10p <marius10p@gmail.com>
2016-05-10 17:30:07 +0100

    new master file, with new options (copy of loopDset)

--- master_file.m
+++ master_file.m
@@ -1,60 +1,103 @@
-addpath('C:\CODE\GitHub\KiloSort')
+addpath(genpath('C:\CODE\GitHub\KiloSort'))
+addpath('D:\DATA\Spikes\EvaluationCode')
 
-ops.Nfilt               = 512 ; % 768 number of filters to use 
-ops.Nfilt0              = 512 ; % 768 number of filters to use 
-ops.nfullpasses         = 6; % 6, number of complete passes through data
-ops.ForceMaxRAMforDat   = Inf; % if you want to force it to use a swap file on disk and less RAM, or no RAM at all (set to 0). 
+% addpath('C:\Users\Marius\Documents\GitHub\npy-matlab')
 
+ops.Nfilt               = 512 ; %  number of filters to use (512, should be a multiple of 32)
+ops.Nrank               = 3;    % matrix rank of spike template model (3)
+ops.nfullpasses         = 6;    % number of complete passes through data during optimization (6)
+ops.whitening           = 'full'; % type of whitening (default 'full', for 'noSpikes' set options for spike detection below)
+ops.maxFR               = 20000;  % maximum number of spikes to extract per batch (20000)
+ops.fs                  = 25000; % sampling rate
+ops.fshigh              = 300;   % frequency for high pass filtering
+ops.NchanTOT            = 129; %384;   % total number of channels
+ops.Nchan               = 120; %374;   % number of active channels 
+ops.ntbuff              = 64;    % samples of symmetrical buffer for whitening and spike detection
+ops.scaleproc           = 200;   % int16 scaling of whitened data
+ops.verbose             = 1;     
+ops.nNeighPC            = 12; %12; % number of channnels to mask the PCs, leave empty to skip (12)
+ops.nNeigh              = 16; % number of neighboring templates to retain projections of (16)
+ops.NT                  = 32*1024+ ops.ntbuff;% this is the batch size, very important for memory reasons. 
+% should be multiple of 32 (or higher power of 2) + ntbuff
 
-% you shouldn't need to change these options
-ops.whitening 	= 'full'; % type of whitening, only full for now
-ops.learnWU     = 1;      % whether to learn templates
-ops.maxFR       = 20000;  % maximum number of spikes to extract per batch
-ops.lambda      = 1e3;    % not used
-ops.Th0         = -7; % not currently used
-ops.ntbuff      = 64;  % samples of symmetrical buffer for whitening and spike detection
-ops.scaleproc   = 200; % int16 scaling of whitened data
-ops.Nrank       = 3;
+% these options can improve/deteriorate results. when multiple values are 
+% provided for an option, the first two are beginning and ending anneal values, 
+% the third is the value used in the final pass. 
+ops.Th               = [4 12 12];    % threshold for detecting spikes on template-filtered data ([6 12 12])
+ops.lam              = [5 5 5];   % large means amplitudes are forced around the mean ([10 30 30])
+ops.nannealpasses    = 4;            % should be less than nfullpasses (4)
+ops.momentum         = 1./[20 400];  % start with high momentum and anneal (1./[20 1000])
+ops.shuffle_clusters = 1;            % allow merges and splits during optimization (1)
+ops.mergeT           = .1;           % upper threshold for merging (.1)
+ops.splitT           = .1;           % lower threshold for splitting (.1)
 
-% these options you should know how to change
-ops.fs          = 25000; % sampling rate
-ops.fshigh      = 300; % high-pass filtering the data above this frequency
-ops.NchanTOT    = 129; % total number of channels
-ops.Nchan       = 120; % number of active channels 
-ops.verbose     = 1;
+ops.nNeighPC    = 12; %12; % number of channnels to mask the PCs, leave empty to skip (12)
+ops.nNeigh      = 32; % number of neighboring templates to retain projections of (16)
 
-% the options might need to be experimented with. 
-% where there are more than one value for a parameter (Th, lam, momentum) these indicate start and end values during optimization + (possibly) the value during final spike extraction
+% new options
+ops.initialize = 'no'; %'fromData'; %'fromData';
 
-ops.Th            = [4 12 10];  % where to set the thresold for spikes 
-ops.lam           = [10 30 30]; % how much to force spikes in the same cluster to have the same amplitude (
-ops.momentum      = [20 400];   % how many detected spikes to average over (start and end values)
-ops.nannealpasses = 4;          % annealing stops after this many passes
+% options for initializing spikes from data
+ops.spkTh           = -6;      % spike threshold in standard deviations (4)
+ops.loc_range       = [3  1];  % ranges to detect peaks; plus/minus in time and channel ([3 1])
+ops.long_range      = [30  6]; % ranges to detect isolated peaks ([30 6])
+ops.maskMaxChannels = 5;       % how many channels to mask up/down ([5])
+ops.crit            = .65;     % upper criterion for discarding spike repeates (0.65)
+ops.nFiltMax        = 10000;   % maximum "unique" spikes to consider (10000)
+dd                  = load('PCspikes2.mat'); % you might want to recompute this from your own data
+ops.wPCA            = dd.Wi(:,1:7);   % PCs 
 
-% feature extraction options. These features are used in the Phy GUI. 
-ops.nNeighPC     = 12; % number of channnels to mask the PCs
-ops.nNeigh       = 16; % number of neighboring templates to retain projections of
+ops.fracse  = 0.1; % binning step along discriminant axis for posthoc merges (in units of sd)
+ops.epu     = Inf;
 
-% data paths
-root        = 'C:\DATA\Spikes';
-% fname   = 'set2\20150924_1_e.dat';
-% fname   = '20141202_all_es.dat';
-fname       = 'set3/20150601_all_s.dat';
-fnameTW     = 'temp_wh.dat'; % will be created. residual of whitened data (not fitting in RAM).
-% if chanMap is a string, it will load the channel configuration from a file 
-% if chanMap is an array, that will indicate the channel order indexing into the raw data channel order
-% This can also be used to drop inactive channels. 
-ops.chanMap = 'forPRBimecToWhisper.mat'; % [1:32]
+ops.showfigures    = 1;
+ops.nSkipCov       = 10; % compute whitening matrix from every N-th batch
+ops.whiteningRange = 32; % how many channels to whiten together (Inf for whole probe whitening, should be fine if Nchan<=32)
 
-%% if you need to reload the data, clear variable 'loaded'
-clear loaded
-load_data_buff; % loads data into RAM + residual data on SSD
-
-%% if you need to rerun the optimization, clear variable 'initialized'
-clear initialized
-run_reg_mu; % iterate the template matching (non-overlapping extraction)
-
+ops.ForceMaxRAMforDat   = 20e9; %0e9;  % maximum RAM the algorithm will try to use
 %%
-fullMPMU; % extracts final spike times (overlapping extraction)
+fidname{1}  = '20141202_all_es';
+fidname{2}  = '20150924_1_e';
+fidname{3}  = '20150601_all_s';
+fidname{4}  = '20150924_1_GT';
+fidname{5}  = '20150601_all_GT';
+fidname{6}  = '20141202_all_GT';
+fidname{7}  = '20151102_1';
+fidname{8}  = 'Loewi20160420_frontal_g0_t0.imec_AP_CAR';
 
+for idset = 6
+    clearvars -except fidname ops idset  tClu tRes time_run dd
+    
+    root        = 'C:\DATA\Spikes';
+    fname       = fullfile(root, sprintf('set%d//%s.dat', idset, fidname{idset}));
+    
+    root        = 'C:\DATA\Spikes';
+    fnameTW     = fullfile('temp_wh.dat'); % (residual from RAM) of whitened data
+    ops.chanMap = 'C:\DATA\Spikes\forPRBimecToWhisper.mat';
+%     ops.chanMap = 'C:\DATA\Spikes\set8\forPRBimecP3opt3.mat';
+    
+    clear loaded
+    % loads data into RAM + residual data on SSD and picks out spikes by a
+    % threshold for initialization
+    load_data_and_PCproject; 
+    
+    % do scaled kmeans to initialize the algorith,
+    if strcmp(ops.initialize, 'fromData')
+        optimizePeaks;      
+    end
 
+    clear initialized
+    run_reg_mu2; % iterate the template matching (non-overlapping extraction)
+    
+    fullMPMU; % extracts final spike times (overlapping extraction)
+
+    % posthoc merge templates
+%     rez = merge_posthoc2(rez);
+    
+    % save here?
+    save(fullfile('C:\DATA\Spikes\rez', sprintf('rez%d.mat', idset)), 'rez');
+      
+    % write to Phy?
+    rezToPhy(rez, fullfile(root, sprintf('set%d', idset)));
+end
+%%
