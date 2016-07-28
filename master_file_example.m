8d9400e5941e4940e57f120f06ed4553569800dc
marius10p <marius10p@gmail.com>
2016-07-28 18:38:07 +0100

    changed paths to config file so that Kilosort doesn't have to be run from inside its folder

--- master_file_example.m
+++ master_file_example.m
@@ -1,24 +1,25 @@
 % default options are in parenthesis after the comment
 
-addpath(genpath('C:\bin\GitHub\KiloSort')) % path to kilosort folder
-addpath(genpath('C:\bin\GitHub\npy-matlab')) % path to npy-matlab scripts
-addpath('configFiles')
-%% 
-run('configFiles/StandardConfig.m')
-tic; % start timer
+addpath(genpath('D:\CODE\GitHub\KiloSort')) % path to kilosort folder
+addpath(genpath('D:\CODE\GitHub\npy-matlab')) % path to npy-matlab scripts
 
-if ops.GPU
-     % initialize GPU (will erase any existing GPU arrays)
-    gpuDevice(1);
+pathToYourConfigFile = 'D:\CODE\Kilosort\configFiles'; % take from Github folder and put it somewhere else (together with the master_file)
+addpath(pathToYourConfigFile)
+
+run(fullfile(pathToYourConfigFile, 'StandardConfig.m'))
+tic; % start timer
+%
+if ops.GPU     
+    gpuDevice(1); % initialize GPU (will erase any existing GPU arrays)
 end
 
 if strcmp(ops.datatype , 'openEphys')
    ops = convertOpenEphysToRawBInary(ops);  % convert data, only for OpenEphys
 end
 %
-[rez, DATA, uproj] = preprocessData(ops);
-rez = fitTemplates(ops, rez, DATA, uproj); 
-rez = fullMPMU(ops, rez, DATA);% extracts final spike times (overlapping extraction)
+[rez, DATA, uproj] = preprocessData(ops); % preprocess data and extract spikes for initialization
+rez = fitTemplates(ops, rez, DATA, uproj);  % fit templates iteratively
+rez = fullMPMU(ops, rez, DATA);% extract final spike times (overlapping extraction)
 
 % posthoc merge templates (under construction)
 %     rez = merge_posthoc2(rez);
