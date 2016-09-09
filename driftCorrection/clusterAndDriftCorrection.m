324341bdbcea05da49fee10ffeda093a1ba4f9a2
marius10p <marius10p@gmail.com>
2016-09-09 10:56:23 +0100

    produces drift maps

--- driftCorrection/clusterAndDriftCorrection.m
+++ driftCorrection/clusterAndDriftCorrection.m
@@ -38,6 +38,7 @@ iCovChans = my_inv(exp(-chanDists/(2*sigDrift^2)), 1e-6);
 
 Nfilt = ops.Nfilt;
 lam = ops.lam(1) * ones(Nfilt, 1, 'single');
+lam = repmat(lam, 3, 1);
 
 U = gpuArray(uBase(itsort(1:Nfilt), :))';
 mu = sum(U.^2,1)'.^.5;
@@ -45,30 +46,47 @@ U = normc(U);
 %
 [uniqy, ~, iqy] = unique(rez.yc);
 
-deltay = zeros(numel(uniqy), numel(indBatch));
+niter = 10;
+deltay = zeros(numel(uniqy), numel(indBatch), niter);
+totdY  = zeros(numel(uniqy), numel(indBatch));
+ 
+alldy = zeros(numel(uniqy), numel(indBatch));
+allNSpikes = zeros(numel(uniqy), numel(indBatch));
 
-for i = 1:10
+cmap = colormap('jet');
+cmap = cmap(round(linspace(1, 64, numel(uniqy))), :);
+
+tic
+%
+for i = 1:niter
     % resample spatial masks up and down
-    Uup   = shift_data(reshape(U, ops.Nchan, []),  sigShift, rez.yc, rez.xc, iCovChans, sigDrift, rez.Wrot);
-    Uup   = reshape(Uup, size(U));
-    Udown = shift_data(reshape(U, ops.Nchan, []), -sigShift, rez.yc, rez.xc, iCovChans, sigDrift, rez.Wrot);
-    Udown = reshape(Udown, size(U));
+    [Uup, Udown] = get_Uupdown(U, sigShift, ops, rez, iCovChans, sigDrift);
         
     mu = repmat(mu, 3, 1);
-    lam = repmat(lam, 3, 1);
     
     dWU = zeros(Nfilt, nProj, 'single');
     nToT = gpuArray.zeros(Nfilt, 1, 'single');
     Cost = gpuArray(single(0));
     %
+   
     for ibatch = 1:numel(indBatch)
         % find clusters
         clips = gpuArray(uproj(indBatch{ibatch}, :))';
         nSpikes = size(clips,2);
+        clips = reshape(clips, ops.Nchan, size(clips,1)/ops.Nchan, []);
+        
+        if i==1
+           % compute spike locations
+           [~, imax]       = max(sq(sum(clips.^2,2)), [], 1); 
+           ySp             = iqy(imax);
+           spikeYs{ibatch} = ySp;
+        else
+           ySp             = spikeYs{ibatch};
+        end
         clips = reshape(clips, ops.Nchan, []);
         
         % resample clips by the delta y
-        clips = shift_data(clips, deltay(:, ibatch), rez.yc, rez.xc, iCovChans, sigDrift, rez.Wrot);        
+        clips = shift_data(clips, totdY(iqy, ibatch), rez.yc, rez.xc, iCovChans, sigDrift, rez.Wrot);        
         clips = reshape(clips, size(U,1), [])';
         
         ci = clips * [Udown U Uup];
@@ -80,27 +98,44 @@ for i = 1:10
         cf           = reshape(cf,[], Nfilt, 3);
         [Mmax, imax] = max(cf, [], 2);
         [~, i3max]   = max(Mmax, [], 3);
-%         keyboard;
     
         % determine added correction to delta y
-        dySpikes = get_dySpikes(Mmax) * sigShift;
+        dySpikes = get_dySpikes(sq(Mmax)) * sigShift;
         
-        
-        
+        % store this correction in a meaningful way
+        [alldy(:,ibatch), allNSpikes(:,ibatch)] = bin_corrections(gather(dySpikes), ySp, numel(uniqy));        
     end
+    
     % smooth out corrections
-    
-    
+    nSmoothSpikes = my_conv2(allNSpikes, [5 5], [1 2]);
+    deltay(:,:,i) = my_conv2(alldy, [5 5], [1 2])./nSmoothSpikes; 
+    totdY = sum(deltay(:,:,1:i), 3);
+    totdY = bsxfun(@minus, totdY, mean(totdY, 2)); 
+
+    for j = 1:size(totdY,1)
+        plot(totdY(j, :), 'Color', cmap(j,:))
+        hold all
+    end
+    hold off
+    drawnow
+    %
     for ibatch = 1:numel(indBatch)
         % shift clips by the new correction
         clips   = gpuArray(uproj(indBatch{ibatch}, :))';
         nSpikes = size(clips,2);
         clips   = reshape(clips, ops.Nchan, []);
-        clips   = shift_data(clips, deltay(:, ibatch), rez.yc, rez.xc, iCovChans, sigDrift, rez.Wrot);        
+        clips   = shift_data(clips, totdY(iqy, ibatch), rez.yc, rez.xc, iCovChans, sigDrift, rez.Wrot);        
         clips   = reshape(clips, size(U,1), [])';
+        %
+        ci = clips * U;
         
+        ci = bsxfun(@plus, ci, (mu(1:end/3) .* lam(1:end/3))');
+        cf = bsxfun(@rdivide, ci.^2, 1 + lam(1:end/3)');
+        cf = bsxfun(@minus, cf, (mu(1:end/3).^2.*lam(1:end/3))');
+        
+        cf           = reshape(cf,[], Nfilt);
         % determine cluster assignment for this iteration
-        [max_cf, id] = max(cf(:,:,2), [], 2);
+        [max_cf, id] = max(cf, [], 2);
         id = gather(id);
         L = gpuArray.zeros(Nfilt, nSpikes, 'single');
         L(id' + [0:Nfilt:(Nfilt*nSpikes-1)]) = 1;
@@ -113,11 +148,10 @@ for i = 1:10
     U = dWU';
     mu = sum(U.^2,1)'.^.5;
     U = normc(U);
-    Cost = Cost/size(inds,2);
+    Cost = Cost/numel(indBatch);
     
     
-    
-    %     disp(Cost)
+    [toc Cost std(totdY(:))]
     
     %     plot(sort(log(1+nToT)))
     %     drawnow
--- driftCorrection/get_Uupdown.m
+++ driftCorrection/get_Uupdown.m
@@ -1,14 +1,8 @@
+function [Uup, Udown] = get_Uupdown(U, sigShift, ops, rez, iCovChans, sigDrift)
+% resample U with a shift up and a shift down (sigShift in um)
+ 
+Uup   = shift_data(reshape(U, ops.Nchan, []),  sigShift, rez.yc, rez.xc, iCovChans, sigDrift, rez.Wrot);
+Uup   = reshape(Uup, size(U));
 
-function [Uup, Udown] = get_Uupdown(U, space_lag, CovChans, Wrot, ops)
-
-Uwh = (Wrot * (CovChans/Wrot)) * U;
-
-
-Udown = reshape(U, 2, ops.Nchan/2, [], size(U,2));
-Udown(:,2:end,:,:)   = Udown(:,1:end-1,:,:);
+Udown = shift_data(reshape(U, ops.Nchan, []), -sigShift, rez.yc, rez.xc, iCovChans, sigDrift, rez.Wrot);
 Udown = reshape(Udown, size(U));
-Udown = normc(Udown);
-Uup = reshape(U, 2, ops.Nchan/2, [], size(U,2));
-Uup(:,1:end-1,:,:) = Uup(:,2:end,:,:);
-Uup = reshape(Uup, size(U));
-Uup = normc(Uup);
