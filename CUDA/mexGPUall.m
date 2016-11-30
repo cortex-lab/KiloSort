bbcbf87d30253b342e4eba636d30a903ca4f0e50
Marius Pachitariu <marius10p@gmail.com>
2016-11-30 16:37:49 +0000

    changed Linux compilation to mexcuda

--- CUDA/mexGPUall.m
+++ CUDA/mexGPUall.m
@@ -1,14 +1,12 @@
 % mexGPUall. For these to complete succesfully, you need to configure the
 % Matlab GPU library first (see README files for platform-specific
 % information)
-if ispc
     mexcuda -largeArrayDims mexMPmuFEAT.cu
     mexcuda -largeArrayDims mexMPregMU.cu
     mexcuda -largeArrayDims mexWtW2.cu
-else
-    mex -largeArrayDims mexMPmuFEAT.cu
-    mex -largeArrayDims mexMPregMU.cu
-    mex -largeArrayDims mexWtW2.cu
-end
+
+%    mex -largeArrayDims mexMPmuFEAT.cu
+%    mex -largeArrayDims mexMPregMU.cu
+%    mex -largeArrayDims mexWtW2.cu
 
 
