9c051b76e3c51e89c9b0527244fb61eeb471cda0
Marius Pachitariu <marius10p@gmail.com>
2017-06-16 20:31:47 +0100

    update Matlab/CUDA to fix "unexpected errors"

--- CUDA/mexGPUall.m
+++ CUDA/mexGPUall.m
@@ -9,4 +9,7 @@
 %    mex -largeArrayDims mexMPregMU.cu
 %    mex -largeArrayDims mexWtW2.cu
 
+% If you get uninterpretable errors like "An unexpected error occurred during CUDA execution", and if you are using Pascal GPUs 
+% (GTX 10xx series), it might be necessary to upgrade to Matlab 2017a / CUDA 8.0. 
+
 
